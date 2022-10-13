77f9c8a5fe6614f889cba06fe571eda01c328645
Eduards A <eduards.abisevs@elev.ga.ntig.se>
2022-10-13 14:08:11 +0200

    1

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 if (process.env.NODE_ENV !== 'production'){
     require('dotenv').config()
 }
-
+// used variables
 const express = require('express')
 const app = express()
 const bcrypt = require('bcrypt')
@@ -10,15 +10,18 @@ const flash = require('express-flash')
 const session = require('express-session')
 const methodOverride = require('method-override')
 
+
+
 const initializePassport = require('./passport-config')
 initializePassport(
     passport, 
     email => users.find(user => user.email === email),
     id => users.find(user => user.id === id)
 )
-
+// local user, saved only on session.
 const users = []
 
+// use variables
 app.set('view-engine', 'ejs')
 app.use(express.urlencoded({ extended: false}))
 app.use(flash())
@@ -31,10 +34,13 @@ app.use(passport.initialize())
 app.use(passport.session())
 app.use(methodOverride('_method'))
 
+//css
+app.use(express.static('public'));
+//render index/ home page when logged in
 app.get('/', checkAuthenticated, (req, res) =>{
     res.render('index.ejs', { name: req.user.name })
 })
-
+//render login page
 app.get('/login', checkNotAuthenticated, (req, res) =>{
     res.render('login.ejs')
 })
@@ -44,11 +50,11 @@ app.post('/login', checkNotAuthenticated, passport.authenticate('local',{
     failureRedirect: 'login',
     failureFlash: true
 }))
-
+//render register page
 app.get('/register', checkNotAuthenticated, (req, res) =>{
     res.render('register.ejs')
 })
-
+//get login info
 app.post('/register', checkNotAuthenticated, async (req, res) => {
     try {
         const hashedPassword = await bcrypt.hash(req.body.password, 10)
@@ -67,6 +73,7 @@ app.post('/register', checkNotAuthenticated, async (req, res) => {
 
 })
 
+//log out function
 app.post('/logout', function(req, res, next){
     req.logout(function(err) {
       if (err) { return next(err); }
@@ -74,6 +81,7 @@ app.post('/logout', function(req, res, next){
     });
   });
 
+//function to check if the user is authenticated
 function checkAuthenticated(req, res, next) {
     if (req.isAuthenticated()){
         return next()
@@ -81,7 +89,7 @@ function checkAuthenticated(req, res, next) {
 
     res.redirect('/login')
 }
-
+//check if the user is not authenticated
 function checkNotAuthenticated(req, res, next) {
     if (req.isAuthenticated()){
         return res.redirect('/')
@@ -89,4 +97,4 @@ function checkNotAuthenticated(req, res, next) {
     next()
 }
 //process.exit();
-app.listen(3000)
+app.listen(5000)
